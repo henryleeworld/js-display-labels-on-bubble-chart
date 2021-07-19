3dcbf6f4ea3eea8be4ab2b9773da0ad66eaef79f
Henry Lee <henryleeworld@gmail.com>
2021-07-20 06:46:35 +0800

    feat:初始化

---
+++ js/bubble-chart.js
@@ -0,0 +1,392 @@
+window.chartColors = {
+    aliceblue: '#F0F8FF',
+    antiquewhite: '#FAEBD7',
+    aqua: '#00FFFF',
+    aquamarine: '#7FFFD4',
+    azure: '#F0FFFF',
+    beige: '#F5F5DC',
+    bisque: '#FFE4C4',
+    black: '#000000',
+    blanchedalmond: '#FFEBCD',
+    blue: '#5A99D3',
+    blueviolet: '#8A2BE2',
+    brown: '#A52A2A',
+    burlywood: '#DEB887',
+    cadetblue: '#5F9EA0',
+    chartreuse: '#7FFF00',
+    chocolate: '#D2691E',
+    coral: '#FF7F50',
+    cornflowerblue: '#6495ED',
+    cornsilk: '#FFF8DC',
+    crimson: '#DC143C',
+    cyan: '#00FFFF',
+    darkblue: '#00008B',
+    darkcyan: '#008B8B',
+    darkgoldenrod: '#B8860B',
+    darkgray: '#A9A9A9',
+    darkgreen: '#006400',
+    darkgrey: '#A9A9A9',
+    darkkhaki: '#BDB76B',
+    darkmagenta: '#8B008B',
+    darkolivegreen: '#556B2F',
+    darkorange: '#FF8C00',
+    darkorchid: '#9932CC',
+    darkred: '#8B0000',
+    darksalmon: '#E9967A',
+    darkseagreen: '#8FBC8F',
+    darkslateblue: '#483D8B',
+    darkslategrey: '#2F4F4F',
+    darkviolet: '#9400D3',
+    deeppink: '#FF1493',
+    deepskyblue: '#00BFFF',
+    dimgrey: '#696969',
+    dodgerblue: '#1E90FF',
+    firebrick: '#B22222',
+    floralwhite: '#FFFAF0',
+    forestgreen: '#228B22',
+    fuchsia: '#FF00FF',
+    gainsboro: '#DCDCDC',
+    ghostwhite: '#F8F8FF',
+    goldenrod: '#DAA520',
+    gold: '#FFD700',
+    green: '#0AA443',
+    greenyellow: '#ADFF2F',
+    grey: '#A3A3A3',
+    honeydew: '#F0FFF0',
+    hotpink: '#FF69B4',
+    indianred: '#CD5C5C',
+    indigo: '#4B0082',
+    ivory: '#FFFFF0',
+    khaki: '#F0E68C',
+    lavender: '#E6E6FA',
+    lavenderblush: '#FFF0F5',
+    lawngreen: '#7CFC00',
+    lemonchiffon: '#FFFACD',
+    lightblue: "#ADD8E6",
+    lightcoral: '#F08080',
+    lightcyan: "#E0FFFF",
+    lightgoldenrodyellow: '#FAFAD2',
+    lightgreen: "#90EE90",
+    lightgrey: "#D3D3D3",
+    lightpink: '#FFB6C1',
+    lightsalmon: '#FFA07A',
+    lightseagreen: "#20B2AA",
+    lightskyblue: "#87CEFA",
+    lightslategrey: "#778899",
+    lightsteelblue: "#B0C4DE",
+    lightyellow: '#FFFFE0',
+    lime: "#00FF00",
+    limegreen: "#32CD32",
+    linen: "#FAF0E6",
+    magenta: '#FF00FF',
+    maroon: "#800000",
+    mediumaquamarine: "#66CDAA",
+    mediumblue: "#0000CD",
+    mediumorchid: '#BA55D3',
+    mediumpurple: '#9370DB',
+    mediumseagreen: "#3CB371",
+    mediumslateblue: '#7B68EE',
+    mediumspringgreen: "#00FA9A",
+    mediumturquoise: "#48D1CC",
+    mediumvioletred: '#C71585',
+    midnightblue: "#191970",
+    mintcream: "#F5FFFA",
+    mistyrose: "#FFE4E1",
+    moccasin: '#FFE4B5',
+    navajowhite: "#FFDEAD",
+    navy: "#000080",
+    oldlace: "#FDF5E6",
+    olive: "#808000",
+    olivedrab: "#6B8E23",
+    orange: '#E97C30',
+    orangered: '#FF4500',
+    orchid: '#DA70D6',
+    palegoldenrod: '#EEE8AA',
+    palevioletred: '#DB7093',
+    palegreen: "#98FB98",
+    paleturquoise: "#AFEEEE",
+    palevioletred: "#DB7093",
+    papayawhip: '#FFEFD5',
+    peachpuff: '#FFDAB9',
+    peru: "#CD853F",
+    pink: '#FFC0CB',
+    plum: '#DDA0DD',
+    powderblue: "#B0E0E6",
+    purple: '#6C18F3',
+    rebeccapurple: '#663399',
+    red: '#CF3F05',
+    rosybrown: "#BC8F8F",
+    royalblue: "#4169E1",
+    saddlebrown: "#8B4513",
+    salmon: '#FA8072',
+    sandybrown: "#f4a460",
+    seagreen: "#2E8B57",
+    seashell: "#FFF5EE",
+    sienna: "#A0522D",
+    silver: "#C0C0C0",
+    skyblue: "#87CEEB",
+    slateblue: '#6A5ACD',
+    slategrey: "#708090",
+    snow: "#FFFAFA",
+    springgreen: "#00FF7F",
+    steelblue: "#4682B4",
+    tan: "#D2B48C",
+    teal: "#008080",
+    thistle: '#D8BFD8',
+    tomato: '#FF6347',
+    turquoise: "#40E0D0",
+    violet: '#EE82EE',
+    wheat: "#F5DEB3",
+    white: "#FFFFFF",
+    whitesmoke: "#F5F5F5",
+    yellow: '#FBBD00',
+    yellowgreen: "#9ACD32"
+};
+var config = {
+    type: 'bubble',
+    data: {
+        datasets: [{
+            backgroundColor: convertHexToRGBA(window.chartColors.tomato, 50),
+            borderColor: window.chartColors.tomato,
+            data: [{
+                    x: -9.349279835390945,
+                    y: 84.61248285322361,
+                    v: 614.8362482853223
+                },
+                {
+                    x: 60.65329218106996,
+                    y: -21.46519204389574,
+                    v: 472.5651577503429
+                },
+                {
+                    x: 7.97067901234567,
+                    y: -22.450274348422496,
+                    v: 661.3125857338821
+                },
+                {
+                    x: -84.06378600823045,
+                    y: -35.009430727023314,
+                    v: 597.7451989026064
+                },
+                {
+                    x: 67.8832304526749,
+                    y: 24.190672153635106,
+                    v: 698.5296639231824
+                },
+                {
+                    x: -52.85493827160494,
+                    y: 38.48336762688615,
+                    v: 380.33264746227707
+                },
+                {
+                    x: 37.05504115226336,
+                    y: 91.2019890260631,
+                    v: 59.820816186556925
+                },
+                {
+                    x: 20.946502057613174,
+                    y: 65.6798696844993,
+                    v: 153.6608367626886
+                },
+                {
+                    x: -17.84722222222223,
+                    y: 45.250342935528124,
+                    v: 78.51937585733882
+                },
+                {
+                    x: 4.007201646090536,
+                    y: -86.75325788751715,
+                    v: 251.0631001371742
+                },
+                {
+                    x: -30.156893004115233,
+                    y: -46.99759945130315,
+                    v: 87.95867626886147
+                },
+                {
+                    x: -37.00617283950617,
+                    y: 47.85065157750344,
+                    v: 5.872770919067215
+                },
+                {
+                    x: 66.79269547325103,
+                    y: 81.12482853223594,
+                    v: 421.47205075445817
+                },
+                {
+                    x: -35.42695473251028,
+                    y: -63.84173525377229,
+                    v: 751.4231824417009
+                },
+                {
+                    x: -60.33179012345679,
+                    y: 96.28429355281207,
+                    v: 412.3928326474623
+                },
+                {
+                    x: 75.41152263374485,
+                    y: 44.83624828532237,
+                    v: 821.0476680384088
+                }
+            ]
+        }, {
+            backgroundColor: convertHexToRGBA(window.chartColors.darkorange, 50),
+            borderColor: window.chartColors.darkorange,
+            data: [{
+                    x: 55.13631687242798,
+                    y: 65.1474622770919,
+                    v: 394.0543552812071
+                },
+                {
+                    x: -37.824074074074076,
+                    y: 40.55126886145405,
+                    v: 548.079561042524
+                },
+                {
+                    x: 79.86368312757202,
+                    y: 54.381001371742116,
+                    v: 699.789951989026
+                },
+                {
+                    x: 91.53292181069958,
+                    y: -10.030006858710564,
+                    v: 265.85219478738
+                },
+                {
+                    x: 80.51697530864197,
+                    y: -69.34842249657065,
+                    v: 662.9329561042524
+                },
+                {
+                    x: -69.85082304526749,
+                    y: -40.240912208504795,
+                    v: 307.69890260631
+                },
+                {
+                    x: -76.23713991769547,
+                    y: -39.374142661179704,
+                    v: 616.8167009602195
+                },
+                {
+                    x: -55.30864197530864,
+                    y: 16.585219478737983,
+                    v: 6.953017832647462
+                },
+                {
+                    x: 76.26800411522635,
+                    y: 10.970507544581622,
+                    v: 894.7745198902605
+                },
+                {
+                    x: 1.8261316872427926,
+                    y: 27.115054869684514,
+                    v: 696.9478737997257
+                },
+                {
+                    x: 4.699074074074076,
+                    y: -51.64780521262003,
+                    v: 830.1397462277091
+                },
+                {
+                    x: -31.779835390946502,
+                    y: 58.01526063100138,
+                    v: 711.016803840878
+                },
+                {
+                    x: -24.277263374485585,
+                    y: 39.43758573388203,
+                    v: 756.2457133058986
+                },
+                {
+                    x: -89.45987654320987,
+                    y: -24.047496570644725,
+                    v: 382.4931412894376
+                },
+                {
+                    x: 56.00565843621399,
+                    y: -49.10665294924554,
+                    v: 6.425754458161866
+                },
+                {
+                    x: 95.45267489711935,
+                    y: 47.59344993141289,
+                    v: 44.710219478738
+                }
+            ]
+        }]
+    },
+    options: {
+        animation: {
+            onComplete: done
+        },
+        aspectRatio: 1,
+        tooltip: false,
+        plugins: {
+            datalabels: {
+                anchor: function(context) {
+                    var value = context.dataset.data[context.dataIndex];
+                    return value.v < 50 ? 'end' : 'center';
+                },
+                align: function(context) {
+                    var value = context.dataset.data[context.dataIndex];
+                    return value.v < 50 ? 'end' : 'center';
+                },
+                font: {
+                    weight: 'bold'
+                },
+                formatter: function(value) {
+                    return Math.round(value.v);
+                },
+                offset: 2,
+                padding: 0
+            },
+            legend: false,
+        },
+        elements: {
+            point: {
+                borderWidth: function(context) {
+                    return Math.min(Math.max(1, context.datasetIndex + 1), 8);
+                },
+
+                hoverBackgroundColor: 'transparent',
+
+                hoverBorderWidth: function(context) {
+                    var value = context.dataset.data[context.dataIndex];
+                    return Math.round(8 * value.v / 1000);
+                },
+
+                radius: function(context) {
+                    var value = context.dataset.data[context.dataIndex];
+                    var size = context.chart.width;
+                    var base = Math.abs(value.v) / 1000;
+                    return (size / 24) * base;
+                }
+            }
+        }
+    }
+};
+
+window.onload = function() {
+    Chart.register(ChartDataLabels);
+    var ctx = document.getElementById('chart-area').getContext('2d');
+    window.bubbleChart = new Chart(ctx, config);
+};
+
+function convertHexToRGBA(hexCode, opacity) {
+    let hex = hexCode.replace('#', '');
+
+    if (hex.length === 3) {
+        hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`;
+    }
+
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    return `rgba(${r},${g},${b},${opacity / 100})`;
+};
+
+function done() {
+    var url = window.bubbleChart.toBase64Image();
+    document.getElementById("chart-url").src = url;
+}
